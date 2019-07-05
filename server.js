b6fef84e3156c003cb0985e14c2ee255a68f485e
Rachit Gupta <rachit371@gmail.com>
2019-07-05 22:57:51 +0530

    add env.port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // Bodyparser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
